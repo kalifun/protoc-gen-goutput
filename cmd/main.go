718fc12ffad025fdfc8720d41bbd9171aee0f7f5
kalifun <kalifun@163.com>
2021-09-30 11:57:21 +0800

    :recycle:  Removal of traditional plug-in method

--- cmd/main.go
+++ cmd/main.go
@@ -1,47 +1,11 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
-
-	"github.com/kalifun/protoc-gen-goutput/generator"
-	_ "github.com/kalifun/protoc-gen-goutput/goutput"
-	"google.golang.org/protobuf/proto"
+	"github.com/kalifun/protoc-gen-goutput/internal/generator"
+	"google.golang.org/protobuf/compiler/protogen"
 )
 
 func main() {
-	gen := generator.New()
-
-	data, err := ioutil.ReadAll(os.Stdin)
-	// filename := "hello.proto"
-	// f, _ := os.Open(filename)
-	// data, err := ioutil.ReadAll(f)
-	if err != nil {
-		gen.Error(err, "read file fail")
-	}
-	if err := proto.Unmarshal(data, gen.Request); err != nil {
-		gen.Error(err, "unmarshal failed")
-	}
-
-	if len(gen.Request.FileToGenerate) == 0 {
-		gen.Fail("no files to generate")
-	}
-
-	gen.CommandLineParameters(gen.Request.GetParameter())
-
-	gen.WrapTypes()
-	gen.SetPackageNames()
-	gen.BuildTypeNameMap()
-	gen.GenerateAllFiles()
-
-	data, err = proto.Marshal(gen.Response)
-	if err != nil {
-		gen.Error(err, "marshal failed")
-	}
-
-	_, err = os.Stdout.Write(data)
-	if err != nil {
-		gen.Error(err, "failed to write")
-	}
-
+	g := generator.Goutput{}
+	protogen.Options{}.Run(g.Generate)
 }
